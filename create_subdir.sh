175e6b15e96a279f586e8821eaaf1ee06d418dfa
kewangtt <kwangbd@connect.ust.hk>
2020-11-14 19:38:25 +0800

    5

--- create_subdir.sh
+++ create_subdir.sh
@@ -1,8 +1,8 @@
 mkdir example/Result
-mkdir example/Result/baselineMap
-mkdir example/Result/CloseForm
+mkdir example/Result/baseline_map
+mkdir example/Result/close_form
 mkdir example/Result/disparity_rgb
 mkdir example/Result/left
 mkdir example/Result/right
-mkdir example/Result/undist_depth
+mkdir example/Result/undisted_depth
 mkdir example/Result/motion_states
